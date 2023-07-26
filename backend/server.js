02ba01245a4172ba0ee16038689e6921b5009e5c
MKYRENE <rodriguezm90@icloud.com>
2023-07-25 22:06:17 -0400

    Started with creating backend routes and server to main page. sample standard message on frontend and sample css.

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+//IMPORTING EXPRESS FROM PACKAGE
+const express = require('express');
+//IMPORT OUR ROUTES 
+const main_routes = require('./routes/main_routes')
+const view_routes = require('./routes/view_routes')
+
+//CREATE THE SERVER PORT NUMBER NOTE: IF PORT IS 
+//IF FALSY MOVE PORT NO. > 3333
+const PORT = process.env.PORT || 3333;
+
+//CREATING SERVER CALLING EXPRESS
+const server = express();
+
+//IMPORTANT MIDDLEWARE NOTE: TO SHARE ANY FILE WITHIN SPECIFIC FOLDER FOR ROUTES
+//GENERATE AUTOMATIC ROUTES TO ANY FILE WITHIN FRONTEND AKA "PUBLIC"
+server.use(express.static('../frontend'))
+
+//LOADING ALL ROUTES >>>>
+server.use('/main', main_routes);
+//ROUTE FOR HOMEPAGE 
+server.use('/', view_routes);
+
+//LAUNCHING SERVER
+server.listen(PORT, () => console.log('Server started on port %s', PORT));
